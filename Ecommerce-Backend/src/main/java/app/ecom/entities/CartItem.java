81e47368285f17220a5de0d3c25d474c4d9cdfa5
Manish.Kumar10 <sangwanmanish2003@gmail.com>
2025-08-13 21:41:05 +0530

    experiment

--- Ecommerce-Backend/src/main/java/app/ecom/entities/CartItem.java
+++ Ecommerce-Backend/src/main/java/app/ecom/entities/CartItem.java
@@ -9,7 +9,7 @@ import lombok.*;
 @NoArgsConstructor
 @AllArgsConstructor
 public class CartItem {
-
+   // hey how are you
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int cartItemId;
