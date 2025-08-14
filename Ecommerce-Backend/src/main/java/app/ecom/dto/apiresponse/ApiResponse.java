18abe0b4fcd6b6a5d356f31f8cf050e4c2f5155c
Akshata <akshatalawand15@gmail.com>
2025-08-14 15:41:21 +0530

    Exception classes added

---
+++ Ecommerce-Backend/src/main/java/app/ecom/dto/apiresponse/ApiResponse.java
@@ -0,0 +1,14 @@
+package app.ecom.dto.apiresponse;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ApiResponse<T> {
+    private int status;     // HTTP status code
+    private String message; // Custom message
+    private T data;         // Generic data type for flexibility
+}
